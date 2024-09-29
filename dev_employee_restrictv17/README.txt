ac0cf942e821f66e2ddfa9ca2d8e09ca9418b007
Offiah <kene.offiah@gmail.com>
2024-09-29 06:40:16 +0100

    hr modules

---
+++ dev_employee_restrictv17/README.txt
@@ -0,0 +1,6 @@
+Employee Restrict:
+=========================================================
+
+Go to Setting / apps and search "Employee / Employee Restrict" and Install
+
+And, you are done with installation. Congratulations!
---
+++ dev_employee_restrictv17/__manifest__.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2015 DevIntelle Consulting Service Pvt.Ltd (<http://www.devintellecs.com>).
+#
+#    For Module Support : devintelle@gmail.com  or Skype : devintelle
+#
+##############################################################################
+
+{
+    'name': 'Employee Access Own Profile',
+    'version': '1.0',
+    'sequence': 1,
+    'category': 'Generic Modules/Human Resources',
+    'description':
+        """
+  odoo Employee Restrict to view other employee details.
+odoo app will Restrict Employee to view other employee details.employee own profile, own access employee, employee own, only employee access, employee own data, employee profile, access own profile, employee own profile, employee self service, employee self details
+
+    """,
+    'summary': 'odoo app will Restrict Employee to view other employee details.employee own profile, own access employee, employee own, only employee access, employee own data, employee profile, access own profile, employee own profile, employee self service, employee self details',
+    'author': 'DevIntelle Consulting Service Pvt.Ltd',
+    'website': 'http://www.devintellecs.com',
+    'depends': ['hr'],
+    'data': ['security/security.xml'],
+    'demo': [],
+    'test': [],
+    'css': [],
+    'qweb': [],
+    'js': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'images': ['images/main_screenshot.png'],
+    'price':9.0,
+    'currency':'EUR',
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
